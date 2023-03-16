9acb8bace92709e194e001971fa7ea204ead7940
Walrussuit101 <tjefferson@ycp.edu>
2023-03-16 10:00:37 -0400

    fix 404 spelling

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
     return (
         <>
             <PageTitle title="Not Found" />
-            <Hero mainText="Not Found" loopTexts={["Sorry.", "My Apologies.", ":(", "So Embarassing..."]}>
+            <Hero mainText="Not Found" loopTexts={["Sorry.", "My Apologies.", ":(", "So Embarrassing..."]}>
                 <Link href="/" className="btn mt-10 uppercase">go home</Link>
             </Hero>
         </>
