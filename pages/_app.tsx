0100c0738c12685122d1d387e103cf7bdeaa7ec7
Walrussuit101 <tjefferson@ycp.edu>
2023-03-17 22:46:07 -0400

    cactus favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/global.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌵</text></svg>"/>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default App;
