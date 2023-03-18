e6c99932138c07ee33f2601f56bf540d993bbae3
Walrussuit101 <tjefferson@ycp.edu>
2023-03-17 22:02:55 -0400

    add navbar dropdown

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="triangle-up border-b-base-300" />
             <footer className="footer footer-center p-4 bg-base-300 text-base-content">
                 <div>
-                    <p>Copyright © Tim Jefferson 2023</p>
+                    <p>Copyright © Tim Jefferson { new Date().getFullYear() }</p>
                 </div>
             </footer>
         </>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,14 +10,30 @@ const NavBar = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 20 20" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
                 <ul className="menu menu-horizontal px-1 hidden lg:flex text-[14px]">
-                    { navLinks.map((navLink, i) => <li key={`nav-link-${i}`}>{navLink}</li>) }
+                    {navLinks.map((navLink, i) => <li key={`nav-link-${i}`}>{navLink}</li>)}
                 </ul>
             </div>
             <div className="navbar-end">
-                <div className="avatar">
-                    <div className="w-12 h-12 rounded-full">
-                        <img src="/headshot.jpg" />
-                    </div>
+                <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="avatar btn btn-circle w-14 h-14">
+                        <div className="w-12 h-12 rounded-full">
+                            <img src="/headshot.jpg" />
+                        </div>
+                    </label>
+                    <ul tabIndex={0} className="menu dropdown-content bg-base-200 rounded-md mt-1 w-44">
+                        <li>
+                            <a className="hover:underline" href="https://github.com/Walrussuit101" target="_blank" rel="noopener noreferrer">
+                                GitHub
+                                <img src="link.svg" className="invert ml-16" height="20" width="20" />
+                            </a>
+                        </li>
+                        <li>
+                            <a className="hover:underline" href="https://www.linkedin.com/in/timothy-jefferson-51a49b181/" target="_blank" rel="noopener noreferrer">
+                                LinkedIn
+                                <img src="link.svg" className="invert ml-[3.15rem]" height="20" width="20" />
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
