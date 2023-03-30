9b89669a9922284037b4b29ad64e1be74fe9aa4b
Walrussuit101 <tjefferson@ycp.edu>
2023-03-29 22:14:22 -0400

    Revert "wire contact form to repo issues"
    This reverts commit 4c715287d5397c1089d9446cfb9ca60b438b4e19.

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,3 @@
-import { ChangeEvent, FormEvent, useState } from "react";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import NavBar from "../components/NavBar";
@@ -7,38 +6,6 @@ import PageTitle from "../components/PageTitle";
 import ProjectsDrawer from "../components/ProjectsDrawer";
 
 const Contact = () => {
-    /*
-     -1: nothing
-     0: failure
-     1: success
-     2: loading
-    */
-    const [status, setStatus] = useState(-1);
-    const [form, setForm] = useState({
-        email: '',
-        message: ''
-    });
-
-    const updateForm = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        });
-    }
-
-    const submit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setStatus(2);
-
-        const res = await fetch('/api/contact', {
-            body: JSON.stringify({ data: form}),
-            method: 'POST'
-        });    
-
-        setStatus(res.ok ? 1 : 0);
-
-        setTimeout(() => setStatus(-1), 3000);
-    }
     return (
         <ProjectsDrawer>
             <NavDrawer>
@@ -47,44 +14,25 @@ const Contact = () => {
                 <Hero mainText="Reach Out" loopTexts={['Have a Question?', "Say Hello!", "See What's New."]} />
                 <div className="flex justify-center w-full my-24">
                     <form
+                        action="https://formspree.io/f/mbjeknqj"
+                        method="POST"
                         className="text px-4 w-full md:w-[40rem]"
-                        onSubmit={submit}
                     >
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email Address</span>
                             </label>
-                            <input 
-                                name="email" 
-                                value={form.email}
-                                onChange={updateForm}
-                                type="email" 
-                                placeholder="" 
-                                className="input input-bordered" 
-                                required 
-                            />
+                            <input name="email" type="email" placeholder="" className="input input-bordered" required />
                         </div>
 
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Message</span>
                             </label>
-                            <textarea 
-                                name="message" 
-                                value={form.message}
-                                onChange={updateForm}
-                                className="textarea textarea-bordered h-60" 
-                                placeholder="" 
-                                required
-                            ></textarea>
+                            <textarea name="message" className="textarea textarea-bordered h-60" placeholder="" required></textarea>
                         </div>
 
-                        <button className="btn w-full uppercase mt-8" disabled={status !== -1} type="submit">
-                            { status === -1 && 'submit' }
-                            { status === 2 && 'submitting...' }
-                            { status === 1 && 'submission success' }
-                            { status === 0 && 'submission failure' }
-                        </button>
+                        <button className="btn w-full uppercase mt-8">submit</button>
                     </form>
                 </div>
                 <Footer />
