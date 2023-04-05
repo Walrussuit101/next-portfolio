f33699b4341b0b923354df6d10e4f706301ca977
Walrussuit101 <tjefferson@ycp.edu>
2023-04-05 09:54:01 -0400

    add loading to indicators to Pokemon Pokedex display

--- components/pokedex/Pokemon.tsx
+++ components/pokedex/Pokemon.tsx
@@ -8,12 +8,17 @@ const p = new Pokedex();
 
 const Pokemon = () => {
     const currentPokemon = useAtomValue(currentPokemonAtom);
+    const [loading, setLoading] = useState(false);
     const [data, setData] = useState<Pokemon | undefined>(undefined);
 
     useEffect(() => {
         const load = async () => {
+            setLoading(true);
             const res = await p.getPokemonByName(currentPokemon)
             setData(res);
+
+            // if we don't have a sprite to load, say we're done loading early
+            if (!res.sprites.front_default) setLoading(false);
         }
 
         if (currentPokemon) load();
@@ -25,12 +30,24 @@ const Pokemon = () => {
                 data &&
                 <>
                     <div className="flex rounded justify-center items-center h-10 bg-base-300 text-white text-xl">
-                        <span className="inline-block capitalize font-mono">{splitDashResourceName(data?.name)}</span>
+                        <span className="inline-block capitalize font-mono">
+                            {loading && 'loading ' + splitDashResourceName(currentPokemon) + '...'}
+                            {!loading && splitDashResourceName(data?.name)}
+                        </span>
                     </div>
                     <div className="flex justify-center items-center">
                         {
                             data?.sprites.front_default &&
-                            <img src={data?.sprites.front_default || ''} alt={data?.name + ' sprite'} height={200} width={200} className="m-4" />
+                            <img 
+                                src={data?.sprites.front_default || ''} 
+                                alt={data?.name + ' sprite'} 
+                                height={200} 
+                                width={200} 
+                                className="m-4" 
+                                style={{fontSize: loading ? '0' : 'inherit'}} // hide alt text when trying to load
+                                onLoad={() => setLoading(false)} // say we're done trying to load when loaded/error
+                                onError={() => setLoading(false)} 
+                            />
                         }
                         {
                             !data?.sprites.front_default &&
@@ -41,7 +58,7 @@ const Pokemon = () => {
                     </div>
                     <div className="flex justify-center items-center gap-1">
                         {
-                            data.types.map(type => <div key={type.type.url} className={`badge badge-lg font-bold capitalize ${type.type.name === 'normal' ? 'text-base-300' : 'text-white'}`} style={{ backgroundColor: getTypeColor(type.type.name) }}>{type.type.name}</div>)
+                            data.types.map(type => <div key={type.type.url} className={`badge badge-lg font-bold capitalize ${loading && 'animate-pulse'} ${type.type.name === 'normal' ? 'text-base-300' : 'text-white'}`} style={{ backgroundColor: getTypeColor(type.type.name) }}>{type.type.name}</div>)
                         }
                     </div>
                     <div className="flex flex-col justify-center items-center gap-y-4 px-10 pt-4">
@@ -49,7 +66,7 @@ const Pokemon = () => {
                         {
                             data.stats.map(stat => {
                                 return (
-                                    <div className="w-full xl:w-2/3" key={stat.stat.url}>
+                                    <div className={`w-full xl:w-2/3 ${loading && 'animate-pulse'}`} key={stat.stat.url}>
                                         <label className="block capitalize">{splitDashResourceName(stat.stat.name)}</label>
                                         <div className="w-full tooltip tooltip-left" data-tip={stat.base_stat}>
                                             <div className="w-full tooltip tooltip-right" data-tip={255}>
