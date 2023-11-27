9bc39d04018fa8cbff57bbed3aa50b72cec1e212
Walrussuit101 <tjefferson@ycp.edu>
2023-11-27 13:30:14 -0500

    remove unused types

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,3 @@
-export interface ProjectLink {
-    link: string
-    title: string
-}
-
-export interface ProjectMdxMeta {
-    title: string
-    links: ProjectLink[]
-}
-
 export interface BoidFieldBounds {
     x: number
     y: number
