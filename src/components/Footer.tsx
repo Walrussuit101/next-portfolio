526821adbd5d9833bceb481598ff6a8ea5fef7f4
Walrussuit101 <tjefferson@ycp.edu>
2023-12-05 15:22:23 -0500

    use client's timezone to determine year in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,23 @@
+'use client';
+import { useEffect, useState } from "react";
+
 const Footer = () => {
+    const [year, setYear] = useState(-1);
+    const isYearSet = year !== -1;
+
+    useEffect(() => {
+        setYear(new Date().getFullYear());
+    }, [])
+
     return (
         <>
             <div className="triangle-up border-b-base-300" />
             <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-                <div>
-                    <p>Copyright © Tim Jefferson { new Date().getFullYear() }</p>
+                <div className="flex gap-1 flex-wrap justify-center">
+                    <span>Copyright © Tim Jefferson</span>
+                    <div className={`w-8 h-5 ${!isYearSet && 'skeleton bg-base-200 rounded'}`}>
+                        {isYearSet && year}
+                    </div>
                 </div>
             </footer>
         </>
