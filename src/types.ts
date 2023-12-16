f1b3f3910f9d34a31d13e373d433d8cfff0510c9
Walrussuit101 <tjefferson@ycp.edu>
2023-12-15 22:50:41 -0500

    add method to get english playlist, and return the medias by group title

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,10 @@ export interface EvoChainDisplay {
     name: string
     stage: number
 }
+
+export interface MediaByGroupTitle {
+    [groupTitle: string]: {
+        name: string
+        location: string
+    }[]
+}
---
+++ src/utils/iptv/index.ts
@@ -0,0 +1,45 @@
+import { M3uParser } from "m3u-parser-generator";
+import { MediaByGroupTitle } from "../../types";
+
+export const getEnglishPlaylists = async () => {
+    // fetch m3u file and parse playlist data from it
+    const res = await fetch('https://iptv-org.github.io/iptv/languages/eng.m3u');
+    const playListRaw = await res.text();
+    const parsedM3UPlaylist = M3uParser.parse(playListRaw);
+
+    // organize playlist contents by group title
+    // put all News media together, Sports, etc.
+    const byGroupTitle: MediaByGroupTitle = {};
+
+    for (let i = 0; i < parsedM3UPlaylist.medias.length; i++) {
+        const media = parsedM3UPlaylist.medias[i];
+        const groupTitleString = media.attributes["group-title"]
+
+        // skip any media missing groupTitle attribute or a name
+        if (groupTitleString === undefined || media.name === undefined) {
+            continue;
+        }
+
+        const groupTitles = groupTitleString.split(';');
+
+        for (let k = 0; k < groupTitles.length; k++) {
+            const groupTitle = groupTitles[k];
+
+            // if we've seen the groupTitle before, push
+            if (groupTitle in byGroupTitle) {
+                byGroupTitle[groupTitle].push({
+                    name: media.name,
+                    location: media.location
+                });
+            } else {
+            // otherwise make a new array
+                byGroupTitle[groupTitle] = [{
+                    name: media.name,
+                    location: media.location
+                }];
+            }
+        }
+    }
+
+    return byGroupTitle;
+}
