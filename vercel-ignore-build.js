1a4fcc98d268e7c92745df9b9234030d25c86f33
Walrussuit101 <tjefferson@ycp.edu>
2023-12-15 23:02:14 -0500

    add vercel ignore build step node script

---
+++ vercel-ignore-build.js
@@ -0,0 +1,6 @@
+// if we're trying to build a preview deployment, make sure it it made with an open PR
+if (process.env['VERCEL_ENV'] === 'preview' && process.env['VERCEL_GIT_PULL_REQUEST_ID'] === '') {
+    process.exit(0);
+} else {
+    process.exit(1);
+}
