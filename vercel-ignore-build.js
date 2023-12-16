f885c9bea660d18848975f6479c413deacfb7b74
Walrussuit101 <tjefferson@ycp.edu>
2023-12-15 23:07:44 -0500

    remove vercel node script

--- vercel-ignore-build.js
+++
@@ -1,9 +0,0 @@
-console.log(process.env['VERCEL_ENV']);
-console.log(process.env['VERCEL_GIT_PULL_REQUEST_ID']);
-
-// if we're trying to build a preview deployment, make sure it it made with an open PR
-if (process.env['VERCEL_ENV'] === 'preview' && process.env['VERCEL_GIT_PULL_REQUEST_ID'] === '') {
-    process.exit(0);
-} else {
-    process.exit(1);
-}
