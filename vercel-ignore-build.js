31dc7aeeebb3c4bb3429eaec88790012c3957052
Walrussuit101 <tjefferson@ycp.edu>
2023-12-15 23:04:48 -0500

    add logs

--- vercel-ignore-build.js
+++ vercel-ignore-build.js
@@ -1,3 +1,6 @@
+console.log(process.env['VERCEL_ENV']);
+console.log(process.env['VERCEL_GIT_PULL_REQUEST_ID']);
+
 // if we're trying to build a preview deployment, make sure it it made with an open PR
 if (process.env['VERCEL_ENV'] === 'preview' && process.env['VERCEL_GIT_PULL_REQUEST_ID'] === '') {
     process.exit(0);
